ee9c8316e99d957ffc95b30b20dfc9dc0414dd40
New User <christiankennedy@Christians-MacBook-Pro.local>
2022-01-14 15:59:26 -0600

    completed assessment

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      class="background-image"
+      alt="Forest Background"
+      src="./assets/Coding-Challenge-Image.png"
+    />
+    <div class="sidebar" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="social-icons">
+      <img alt="Facebook Logo" src="./assets/Facebook.svg" />
+      <br/>
+      <img alt="Facebook Logo" src="./assets/Instagram.svg" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
+html {
+  width: 100%;
+}
+body {
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position:relative;
+  overflow: hidden;
+}
+.background-image {
+  /* height: 100vh; */
+  max-width: 100vw;
+  min-width: 1200px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: -5;
+}
+.sidebar {
+  background-color: #611818;
+  height: 100vh;
+  width: 25vw;
+}
+.social-icons {
+  position:absolute;
+  right: 20px;
+  bottom: 20px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,161 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 class="title" :class="{ active: isActive }">Explore</h1>
+    <div class="subtitle-box">
+      <div>
+        <div class="subtitle-expandable" :class="{ active: isActive }">
+          <div
+            v-on:click="toggleIsActive()"
+            class="toggle-button"
+            :class="{ active: isActive }"
+          >
+            <img
+              src="../assets/Plus.svg"
+              class="button-svg"
+              :class="{ active: isActive }"
+            />
+          </div>
+          <div class="inner-content" :class="{ active: isActive }">
+            <h2 class="inner-header">Explore</h2>
+            <hr class="inner-hr"/>
+            <br/>
+            <div class="inner-filler" :class="{ active: isActive }">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+              </div>
+              <br/>
+            <div class="inner-button-background">
+              READ MORE
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="subtitle-text" :class="{ active: isActive }">
+        More Details
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    detailsOpen: Boolean,
+  },
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    toggleIsActive() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.active {
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.title {
+  position: absolute;
+  left: 10vw;
+  top: 40vh;
+  color: white;
+  font-size: 150px;
+  font-weight: 500;
+  letter-spacing: 0.05em;
+  transition: all 0.5s ease-in-out;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.title.active {
+  opacity: 0;
+  margin-top: 30px;
 }
-a {
-  color: #42b983;
+.subtitle-box {
+  display: flex;
+  position: absolute;
+  top: 78vh;
+  left: 12vw;
+}
+.subtitle-text {
+  color: white;
+  font-size: 20px;
+  padding-left: 60px;
+  padding-top: 20px;
+  transition: all 0.5s ease-in-out;
+}
+.subtitle-text.active {
+  opacity: 0;
+  margin-top: 30px;
+}
+.subtitle-expandable {
+  position: absolute;
+  border-radius: 100px;
+  text-align: right;
+  left: 10px;
+  top: 20px;
+  transition: all 0.5s ease-in-out;
+  background-color: white;
+  width: 10px;
+  height: 10px;
+}
+.subtitle-expandable.active {
+  border-radius: 0;
+  width: 50vw;
+  height: 75vh;
+  top: -60vh;
+  background-color: white;
+}
+.toggle-button {
+  transition: all 0.5s ease-in-out;
+  margin-left: -10px;
+  margin-top: -10px;
+  text-align: right;
+}
+.toggle-button.active {
+  margin-left: 0px;
+  margin-top: 0px;
+}
+.button-svg {
+  transition: all 0.5s ease-in-out;
+}
+.button-svg.active {
+  transform: rotate(45deg);
+}
+.inner-content {
+  transition: all 0.1s ease-in-out;
+  opacity: 0;
+  text-align: left;
+  padding: 20px 60px;
+  height: 0px;
+}
+.inner-content.active {
+  transition: all 1s ease-in-out;
+  opacity: 1;
+  height: auto;
+  overflow: auto;
+}
+.inner-header {
+  font-size: 50px;
+  color: #611818;
+  margin: 0px;
+}
+.inner-hr {
+  height: 2px;
+  border: none;
+  background: rgb(97,24,24);
+  background: linear-gradient(90deg, rgba(97,24,24,1) 0%, rgba(97,24,24,1) 10%, rgba(255,255,255,1) 65%);
+}
+.inner-button-background {
+  max-width: 150px;
+  text-align: center;
+  padding: 10px;
+  color: white;
+  background: rgb(97,24,24);
+  background: linear-gradient(90deg, rgba(97,24,24,1) 0%, rgba(163,103,84,1) 100%);
+}
+.inner-filler {
+  font-size: 13px;
 }
 </style>
